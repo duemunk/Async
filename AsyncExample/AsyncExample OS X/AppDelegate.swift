8d109877adf6eceb4f30e35487c17c535b236ce1
Tobias Due Munk <tobias@developmunk.dk>
2015-02-09 23:36:31 +0100

    Initial Swift 1.2 support

--- AsyncExample/AsyncExample OS X/AppDelegate.swift
+++ AsyncExample/AsyncExample OS X/AppDelegate.swift
@@ -12,11 +12,11 @@ class AppDelegate: NSObject, NSApplicationDelegate {
                             
 
 
-	func applicationDidFinishLaunching(aNotification: NSNotification?) {
+	func applicationDidFinishLaunching(aNotification: NSNotification) {
 		// Insert code here to initialize your application
 	}
 
-	func applicationWillTerminate(aNotification: NSNotification?) {
+	func applicationWillTerminate(aNotification: NSNotification) {
 		// Insert code here to tear down your application
 	}
 
