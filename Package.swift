71415adae45d498da9905a7fb2aef2a49ff6c6ab
Cole Campbell <12163026+colemcampbell@users.noreply.github.com>
2019-10-30 01:32:48 -0500

    Configure Swift package for Swift 5.1

--- Package.swift
+++ Package.swift
@@ -1,3 +1,4 @@
+// swift-tools-version:5.1
 //
 //  Async.swift
 //
@@ -23,9 +24,21 @@
 //	IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
 //	CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-
 import PackageDescription
 
 let package = Package(
-    name: "Async"
+    name: "Async",
+    platforms: [
+        .macOS(.v10_10),
+        .iOS(.v8),
+        .watchOS(.v3),
+        .tvOS(.v9),
+    ],
+    products: [
+        .library(name: "Async", targets: ["Async"]),
+    ],
+    targets: [
+        .target(name: "Async"),
+        .testTarget(name: "AsyncTests", dependencies: ["Async"])
+    ]
 )
